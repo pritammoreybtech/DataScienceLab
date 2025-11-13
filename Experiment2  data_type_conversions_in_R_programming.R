70fd5e18be556617a2cdbb8f74fc1f8604230d17
pritammoreybtech <pritam.morey.btech2022@sitnagpur.siu.edu.in>
2025-11-13 10:32:43 +0530

    Update Experiment2  data_type_conversions_in_R_programming.R

--- Experiment2  data_type_conversions_in_R_programming.R
+++ Experiment2  data_type_conversions_in_R_programming.R
@@ -1,3 +1,15 @@
+#AIM:
+
+#Perform and demonstrate data type conversions in R programming.  
+#a) Create a vector called monster, storing the values T, T, T, F, T, T, T, T, T, T.  
+#b) Create the character vector yugioh vector. Check the type of yugioh.  
+#c) Combine the monster and yugioh vector. Check its type.  
+#d) Create atk numerical vector and combine atk and monster into a vector called coerce.check. Check its type and print it, if you need.
+#e) Explicit Type Conversion Using as.*() Functions  
+#f) Demonstrate label encoding/one-hot encoding in Python.  
+
+#SOLUTION
+
 #a)Create a vector called monster, storing the values T, T, T, F, T, T, T, T, T, T.
 monster<-c(TRUE,TRUE,TRUE,FALSE,TRUE,TRUE,TRUE,TRUE,TRUE,TRUE)
 print(monster)
@@ -38,6 +50,7 @@ print(char_yugioh)
 log_atk <- as.logical(atk)
 print("Numeric to Logical:")
 print(log_atk)
+
 
 
 
