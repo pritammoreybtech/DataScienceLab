067d9a3e5bfc15ebf2c1d50cb7579a28cfbeb278
pritammoreybtech <pritam.morey.btech2022@sitnagpur.siu.edu.in>
2025-11-13 10:43:30 +0530

    Update Experiment9 to_load_and_explore_data_in_R_using_tibbles.R

--- Experiment9 to_load_and_explore_data_in_R_using_tibbles.R
+++ Experiment9 to_load_and_explore_data_in_R_using_tibbles.R
@@ -1,3 +1,20 @@
+#AIM:
+
+#To load and explore data in R using tibbles, perform descriptive analysis, fit a linear regression model, visualize the regression line, and evaluate model performance using statistical metrics such as R-squared and p-values. 
+#Load the libraries you will need in R programming  
+#a)Load the data as a tibble (readr has a function that does this directly)  
+#b)Get the descriptives for your data so you can understand what you're dealing with a little better   
+#c) Explore the data and see if there are any interesting trends to consider   
+#d) Define the linear model   
+#e) Plot the regression line   
+#f) Print the results of the model   
+#g) How many observations was the regression run on?   
+#h) What is the R-squared of this regression? What does it tell you?  
+#i) Determine if size is a statistically significant predictor of price.   
+#j) What is the regression equation associated with this regression model?   
+
+#SOLUTION:
+
 library(tidyverse)
 library(readr)
 
@@ -64,6 +81,7 @@ slope_size <- model_summary$coefficients[2, 1]
 
 cat("Regression Equation:\n")
 cat("Price =", round(intercept, 2), "+ (", round(slope_size, 4), " * Size)\n")
+
 
 
 
